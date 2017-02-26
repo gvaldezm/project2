4193f1e25078b856ddada858286e1119fb4973e8
Gabriel Valdez <gvaldezm@gmail.com>
2017-02-25 20:41:26 -0800

    Add movie chooser with radio option

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 $(function() {
+  $(".clearcl").hide();
   $("button").click(function(event){
   var ageInput=parseInt($("input#age").val());
   var nameInput=$("input#name").val();
+  var movieTypeInput=$("input:radio[name=movieType]:checked").val();
   console.log (ageInput);
   console.log (nameInput);
-  alert("Hi " + nameInput + " we'll show you some rides for you")
+  console.log (movieTypeInput);
+  alert("Hi " + nameInput + " we'll show you some movies for you")
 $(".clearcl").hide();
   if (ageInput>=18){
     $(".moviesover").show();
